384872ec0ccd47f32c84c00b39e7c238ec02a4bc
Martin Vacas <martin@MacBook-Air-de-Martin.local>
2019-04-22 20:05:06 -0300

    Se agrega progress bar

--- app/src/main/java/ar/com/tesina/climapp/MainActivity.java
+++ app/src/main/java/ar/com/tesina/climapp/MainActivity.java
@@ -118,15 +118,11 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
-
-
+    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        /* Use AppCompatActivity's method getMenuInflater to get a handle on the menu inflater */
         MenuInflater inflater = getMenuInflater();
-        /* Use the inflater's inflate method to inflate our menu layout to this menu */
         inflater.inflate(R.menu.forecast, menu);
-        /* Return true so that the menu is displayed in the Toolbar */
         return true;
     }
 
