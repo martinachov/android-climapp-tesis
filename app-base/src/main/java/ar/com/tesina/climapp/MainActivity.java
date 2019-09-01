57b90198ab5e0a3557763373f858eb652df4582e
Matias Garcia <matias.garcia@fluxit.com.ar>
2019-09-01 19:03:54 -0300

    Se agrega nuevo menu

--- app-base/src/main/java/ar/com/tesina/climapp/MainActivity.java
+++ app-base/src/main/java/ar/com/tesina/climapp/MainActivity.java
@@ -4,17 +4,22 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -27,21 +32,44 @@ import ar.com.tesina.climapp.data.WeatherContract;
 import ar.com.tesina.climapp.sync.ClimappSyncUtils;
 import ar.com.tesina.climapp.utilities.MyPagerAdapter;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
     private FragmentPagerAdapter adapterViewPager;
 
+    private DrawerLayout drawerLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
-        getSupportActionBar().setElevation(0f);
+      //  getSupportActionBar().setElevation(0f);
 
         ViewPager vpPager = (ViewPager) findViewById(R.id.vpPager);
         adapterViewPager = new MyPagerAdapter(getSupportFragmentManager());
         vpPager.setAdapter(adapterViewPager);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 
     /**
@@ -65,19 +93,45 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-
-
-    /**
-     *
-     * @param menu
-     * @return
-     */
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.forecast, menu);
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        int title;
+
+        Log.i("aaaaaaa", menuItem.getTitle().toString());
+//        switch (menuItem.getItemId()) {
+//            case R.id.nav_camera:
+//                title = R.string.menu_tools;
+//                break;
+//            case R.id.nav_manage:
+//                title = R.string.menu_tools;
+//                break;
+//            case R.id.nav_share:
+//                title = R.string.menu_share;
+//                break;
+//            case R.id.nav_send:
+//                title = R.string.menu_send;
+//                break;
+//            default:
+//                throw new IllegalArgumentException("menu option not implemented!!");
+//        }
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+
         return true;
     }
+
+
+//    /**
+//     *
+//     * @param menu
+//     * @return
+//     */
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        MenuInflater inflater = getMenuInflater();
+//        inflater.inflate(R.menu.forecast, menu);
+//        return true;
+//    }
 
     /**
      *
